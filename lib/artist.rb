ef578e1ab9c8abdecc97ee43d7e5153c6e346aed
Samuel Cornet <samuel@noukod.com>
2018-10-10 17:34:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,21 +27,21 @@ end
 
 # class Artist
 #   attr_accessor :name
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @songs = []
 #   end
-# 
+#
 #   def add_song(song)
 #     @songs << song
 #     song.artist = self
 #   end
-# 
+#
 #   def songs
 #     @songs
 #   end
-# 
+#
 #   def genres
 #     @songs.collect { |song| song.genre }
 #   end

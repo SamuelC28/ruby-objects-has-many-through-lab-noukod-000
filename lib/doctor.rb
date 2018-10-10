7c20ecab2da0211fb821bc2be931ce6911edf8be
Samuel Cornet <samuel@noukod.com>
2018-10-10 17:36:09 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,21 +1,46 @@
-class Doctor
-  attr_accessor :name
+# class Doctor
+#   attr_accessor :name
+# 
+#   def initialize(name)
+#     @name = name
+#     @appointments = []
+#   end
+# 
+#   def add_appointment(appointment)
+#     @appointments << appointment
+#     appointment.doctor = self
+#   end
+# 
+#   def appointments
+#     @appointments
+#   end
+# 
+#   def patients
+#     @appointments.collect { |appointment| appointment.patient }
+#   end
+# end
 
+class Doctor
+  attr_reader :name, :appointment, :patient
+  @@all_doc = []
   def initialize(name)
     @name = name
-    @appointments = []
+    @@all_doc << self
   end
-
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.doctor = self
+  def self.all
+    @@all_doc
   end
-
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
   def appointments
-    @appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
   end
-
   def patients
-    @appointments.collect { |appointment| appointment.patient }
+    appointments.map do |appointment|
+      appointment.patients
+    end
   end
 end
